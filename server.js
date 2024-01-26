1734ef22bc398219f19f1ff1fcab5ce6f763ed07
Jay Lim <jlimoman@gmail.com>
2024-01-26 15:43:29 -0700

    basic express server

--- server.js
+++ server.js
@@ -5,4 +5,29 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv');
 
+const app = express();
+app.use(express.json());
+app.use(cors());
 
+const PORT = process.env.PORT || 5000;
+
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, {
+  userNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// define MDB schema and models
+
+// define route for user auth and CRUD
+
+app.listen(PORT, () => {
+  console.log('Server is running on port ${PORT}');
+})
