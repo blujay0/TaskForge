81787eca4a6326cfe927714b257cd608d9725e34
Jay Lim <jlimoman@gmail.com>
2024-01-26 18:31:21 -0700

    connect to MDB

--- server/server.js
+++ server/server.js
@@ -11,6 +11,9 @@ const MONGODB_URI = process.env.MONGODB_URI;
 const JWT_SECRET = process.env.JWT_SECRET;
 
 // connect to MongoDB
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
 
 // define user schema and model
 
