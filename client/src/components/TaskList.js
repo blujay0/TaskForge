6ebe5ff3befc6243d12aa4bf956b16b17e8d2ecc
Jay Lim <jlimoman@gmail.com>
2024-01-26 17:48:06 -0700

    TaskList return

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -5,7 +5,17 @@ const TaskList = () => {
   const [tasks, setTasks] = useState([]);
   const [error, setError] = useState('');
 
-
+  return (
+    <div>
+      <h2>Task List</h2>
+      {error && <div>{error}</div>}
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TaskList;
