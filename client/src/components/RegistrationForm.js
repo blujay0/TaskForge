d8e06e73df371b9e607f68c6859637537873a503
Jay Lim <jlimoman@gmail.com>
2024-01-26 17:38:09 -0700

    states for Register component

---
+++ client/src/components/RegistrationForm.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const RegistrationForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+
+  
+}
