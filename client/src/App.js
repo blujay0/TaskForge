ed2ea81a72dd91c64ae4342f0aec98eed2c279b7
Jay Lim <jlimoman@gmail.com>
2024-01-26 19:27:06 -0700

    edit App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
+import TaskManager from './components/TaskManager';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Task Manager App</h1>
+      <LoginForm />
+      <hr />
+      <RegisterForm />
+      <hr />
+      <TaskManager />
     </div>
   );
-}
+};
 
 export default App;
