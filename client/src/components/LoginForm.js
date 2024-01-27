596f28def2f72ebf9b8984244ca68932e9c912c6
Jay Lim <jlimoman@gmail.com>
2024-01-26 19:04:41 -0700

    create models/user.js

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { loginUser } from '../services/api';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Username:</label>
+          <input 
+            type="text"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm;
