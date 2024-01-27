3299e29985dd6b696952e7513dc6540d5c63057f
Jay Lim <jlimoman@gmail.com>
2024-01-26 17:40:48 -0700

    registration form

--- client/src/components/RegistrationForm.js
+++ client/src/components/RegistrationForm.js
@@ -7,5 +7,24 @@ const RegistrationForm = () => {
   const [error, setError] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
 
-  
-}
+  return (
+    <div>
+      <h2>Register</h2>
+      {error && <div>{error}</div>}
+      {successMessage && <div>{successMessage}</div>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={handleEmailChange} required />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={handlePasswordChange} required/>
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegistrationForm;
