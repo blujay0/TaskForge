9aff1fcfd5cf8dc1fa56c19a8527f8686cb98e34
Jay Lim <jlimoman@gmail.com>
2024-01-26 17:45:12 -0700

    TaskList states

---
+++ client/src/components/TaskList.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState('');
+
+
+};
+
+export default TaskList;
