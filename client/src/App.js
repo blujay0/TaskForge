9a15fce876cce581bafddd1130cdf02d5a6c1fc4
Jay Lim <jlimoman@gmail.com>
2024-01-26 20:41:31 -0700

    minor App.js edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,36 @@
 import React, { useState } from 'react';
-import TaskManager from './components/TaskManager';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
+import TaskManager from './components/TaskManager';
 
 const App = () => {
   const [authenticated, setAuthenticated] = useState(false);
-  const [showLoginForm, setShowLoginForm] = useState(true); // Initial state: show login form
+  const [showLoginForm, setShowLoginForm] = useState(true);
+  const [showRegisterForm, setShowRegisterForm] = useState(false);
 
   const handleLogin = () => {
     setAuthenticated(true);
+    setShowLoginForm(false);
+    setShowRegisterForm(false);
+  };
+
+  const handleLogout = () => {
+    setAuthenticated(false);
+    setShowLoginForm(true);
+    setShowRegisterForm(false);
   };
 
   const handleRegister = () => {
-    setShowLoginForm(true); // Show the login form after registering
-  };
-
-  const toggleForm = () => {
-    setShowLoginForm((prevShowLoginForm) => !prevShowLoginForm); // Toggle between login and register forms
+    setShowLoginForm(false);
+    setShowRegisterForm(true);
   };
 
   return (
     <div>
       <h1>Task Manager</h1>
-      {!authenticated && showLoginForm && <LoginForm onLogin={handleLogin} setAuthenticated={setAuthenticated} />}
-      {!authenticated && !showLoginForm && <RegisterForm onRegister={handleRegister} />}
-      {authenticated && <TaskManager />}
-      <button onClick={toggleForm}>Switch Form</button> {/* button to switch between login and register forms */}
+      {!authenticated && showLoginForm && <LoginForm onLogin={handleLogin} onRegister={handleRegister} setAuthenticated={setAuthenticated} />}
+      {!authenticated && showRegisterForm && <RegisterForm onRegister={handleRegister} setAuthenticated={setAuthenticated} />}
+      {authenticated && <TaskManager onLogout={handleLogout} />}
     </div>
   );
 };
