6f06d3f1e20ab8fa6d09534539aa387920f95738
Jay Lim <jlimoman@gmail.com>
2024-02-06 21:35:19 -0700

    create controllers

---
+++ server/controllers/notesController.js
@@ -0,0 +1,67 @@
+const fetchNotes = async(req, res) => {
+  // find the notes
+  const notes = await Note.find();
+  // respond with the notes
+  res.json({notes: notes })
+}
+
+const fetchNote = async (req, res) => {
+  // get id off the url
+  const noteId = req.params.id;
+
+  // find the note using that id
+  const note = await Note.findById(noteId);
+
+  // respond with the note
+  res.json({note: note})
+}
+
+const createNote = async(req, res) {
+  async (req, res) => {
+    // get sent in data off req body
+    const title = req.body.title;
+    const body = req.body.body;
+  
+    // create a note
+    const note = await Note.create({
+      title: title,
+      body: body,
+    });
+  
+    // respond with the new note
+    res.json({note: note});
+  }
+}
+
+const updateNote = async(req, res) => {
+  // get the id off the url
+  const noteId = req.params.id;
+
+  // get the data off the request body
+  const title = req.body.title;
+  const body = req.body.body;
+
+  // find and update the record
+  await Note.findByIdAndUpdate(noteId, {
+    title: title,
+    body: body,
+  });
+
+  // find updated note
+  const note = await Note.findById(noteId);
+
+  // respond with it
+  res.json({note: note});
+}
+
+const deleteNote = async(req, res) => {
+  // get id off url
+  const noteId = req.params.id;
+
+  // delete the record
+  await Note.deleteOne({id: noteId}); 
+
+  // respond
+  res.json({success: "Record deleted"});
+};
+
--- server/server.js
+++ server/server.js
@@ -19,78 +19,19 @@ app.use(express.json());
 connectToDb();
 
 // routing
-app.get('/', (req, res) => {
-  res.json({hello: "world"})
-})
-
 // fetch all notes
-app.get('/notes', async (req, res) => {
-  // find the notes
-  const notes = await Note.find();
-  // respond with the notes
-  res.json({notes: notes })
-})
+app.get('/notes', )
 
 // fetch single note
-app.get('/notes/:id', async (req, res) => {
-  // get id off the url
-  const noteId = req.params.id;
-
-  // find the note using that id
-  const note = await Note.findById(noteId);
-
-  // respond with the note
-  res.json({note: note})
-})
+app.get('/notes/:id', )
 
 // create note
-app.post('/notes', async (req, res) => {
-  // get sent in data off req body
-  const title = req.body.title;
-  const body = req.body.body;
+app.post('/notes', )
 
-  // create a note
-  const note = await Note.create({
-    title: title,
-    body: body,
-  });
-
-  // respond with the new note
-  res.json({note: note});
-})
-
-app.put('/notes/:id', async(req, res) => {
-  // get the id off the url
-  const noteId = req.params.id;
-
-  // get the data off the request body
-  const title = req.body.title;
-  const body = req.body.body;
-
-  // find and update the record
-  await Note.findByIdAndUpdate(noteId, {
-    title: title,
-    body: body,
-  });
-
-  // find updated note
-  const note = await Note.findById(noteId);
-
-  // respond with it
-  res.json({note: note});
-})
+app.put('/notes/:id', )
 
 // delete
-app.delete('/notes/:id', async(req, res) => {
-  // get id off url
-  const noteId = req.params.id;
-
-  // delete the record
-  await Note.deleteOne({id: noteId}); 
-
-  // respond
-  res.json({success: "Record deleted"});
-})
+app.delete('/notes/:id', )
 
 // start server
 app.listen(process.env.PORT);
