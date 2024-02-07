c749d00601335b3cfe8aa97ffdc3bb7b584e515c
Jay Lim <jlimoman@gmail.com>
2024-02-06 20:40:01 -0700

    connect to db

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
+// load env vars
+// wrap in if statement to run only for local env use
+if (process.env.NODE_ENV != 'production') {
+  require("dotenv").config();
+}
+
 // import dependencies
 const express = require("express");
+const connectToDb = require('./config/connectToDb');
 
 // create an express app
 const app = express();
+
+// connecty to db
+connectToDb();
 
 // routing
 app.get('/', (req, res) => {
@@ -10,4 +20,4 @@ app.get('/', (req, res) => {
 })
 
 // start server
-app.listen(3000);
+app.listen(process.env.PORT);
